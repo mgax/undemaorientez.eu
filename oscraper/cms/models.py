fa98eb6fe53be900eef176c0aed2c0e9d24a88e3
Alex Morega <alex@grep.ro>
2025-06-18 22:41:07 +0300

    Event model

--- oscraper/cms/models.py
+++ oscraper/cms/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
@@ -7,5 +8,21 @@ class HomePage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel("intro"),
+        "intro",
+    ]
+
+
+class Event(Page):
+    organiser = models.CharField(max_length=1000)
+    location = models.CharField(max_length=1000, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    original_text = models.TextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        "organiser",
+        "location",
+        "start_date",
+        "end_date",
+        FieldPanel("original_text", read_only=True),
     ]
